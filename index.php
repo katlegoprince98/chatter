ced670bbf09331f18cf675bfcc9a72290c35fb24
katlegoprince98 <CynicsTech1998@gmail.com>
2023-02-02 18:08:28 -0800

    codes in index page

--- index.php
+++ index.php
@@ -17,7 +17,33 @@
                     This is an error
                 </div>
                 <div class="name-details">
-                    
+                    <div class="field">
+                        <label for="name">First Name</label>
+                        <input type="text" name ="name" id="name" placeholder="Enter your first name">
+                    </div>
+                    <div class="field">
+                        <label for="lname">Last Name</label>
+                        <input type="text" name ="lname" id="lname" placeholder="Enter your last name">
+                    </div>
+                    <div class="field">
+                        <label for="name">E-mail address</label>
+                        <input type="email" name ="email" id="email" placeholder="Enter your E-mail address">
+                    </div>
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" name ="password" id="password" placeholder="Enter your password">
+                    </div>
+                    <div class="field">
+                        <label for="password2">Confirm password</label>
+                        <input type="password" name ="password2" id="password2" placeholder="Confirm your password">
+                    </div>
+                    <div class="field">
+                        <label>Select image</label>
+                        <input type="file">
+                    </div>
+                    <div class="field">
+                        <input type="submit" name="submit" id="submit" value="CREATE">
+                    </div>
                 </div>
             </form>
         </section>
