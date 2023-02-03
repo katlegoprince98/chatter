992fa8961e687f0c41e2aa880af774e23a923aa0
katlegoprince98 <CynicsTech1998@gmail.com>
2023-02-02 17:49:43 -0800

    pages

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+    <title>Chatter</title>
+</head>
+<body>
+    
+</body>
+</html>
