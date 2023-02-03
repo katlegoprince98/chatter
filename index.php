bee48c383163b5293ddbb4c37ea0a52bc01c62a0
katlegoprince98 <CynicsTech1998@gmail.com>
2023-02-02 17:56:39 -0800

    cont

--- index.php
+++ index.php
@@ -6,12 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
-    <title>Chatter</title>
+    <title>KAVIA</title>
 </head>
 <body>
     <div class="wrapper">
         <section class="form signup">
             <header>Chatter</header>
+            <form action="">
+                <div class="eror-txt">
+                    This is an error
+                </div>
+                <div class="name-details">
+                    
+                </div>
+            </form>
         </section>
     </div>
     
